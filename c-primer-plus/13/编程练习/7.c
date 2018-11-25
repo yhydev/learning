8ded8b88265036fd9df613f9519d564200576168
yhydev <yhydev@foxmail.com>
2018-11-25 22:51:04 +0800

    chapter13 update

--- c-primer-plus/13/编程练习/7.c
+++ c-primer-plus/13/编程练习/7.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <unistd.h>
 int main(int argc,char *argv[]){
 	
+	const size_t bufsize = 1024;	
 	FILE *f1;
 	FILE *f2;
+	char buf[bufsize];
+	
+	int readbytes = 1;
 
 	if(argc < 3){
-		fprintf(stderr, "Useage: %s <fname1> <fname2>\n". argv[0]);
-		exit(EXIT_SUCCESS);
+		fprintf(stderr, "Useage: %s <fname1> <fname2>\n",  argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	f1 = fopen(argv[1],'r');
+	f1 = fopen(argv[1], "r");
 	if(f1 == NULL){
 		fprintf(stderr, "Can't open %s\n",argv[1]);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 
-	f2 = fopen(argv[2],'r');
+	f2 = fopen(argv[2], "r");
 	if(f2 == NULL){
 		fprintf(stderr, "Can't open %s\n",argv[2]);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 	
+	if(setvbuf(f1, buf, _IOLBF, bufsize) != 0){
+		puts("set _IOLBF fail");
+		exit(EXIT_FAILURE);
+	}
 	
-	
+	while(readbytes){
+		while(0 < (readbytes = fread(buf, 1, bufsize, f1))){
+		//	readbytes = fread(buf, 1, bufsize, f1);
+		//	fprintf(stdout,"%s - %s | of %d bytes", argv[1], buf, readbytes);
+		//	sleep(2);
+			fwrite(buf, 1, readbytes, stdout);
+		}	
 
+	}
+
+	fclose(f1);
+	fclose(f2);
+	
+	return 0;
 }
--- c-primer-plus/13/编程练习/buff.c
+++ c-primer-plus/13/编程练习/buff.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 int main(int argc, char *argv[]){
-
-	char buff[1024];
-	//char ipt[20];
-	int i = 0x7fffffff;
+	const unsigned bufsize = 1024;
+	char buf[bufsize];
 
 	//setvbuf(stdout, buff, _IOFBF, 1024);
 	
-	setvbuf(stdout, buff, _IOLBF, 1024);
+	setvbuf(stdout, buf, _IOLBF, bufsize);
 	
 
-	fprintf(stdout, "123\n");
-	while(i--){}
-//	printf("123");
-//	sleep(5);
-//	scanf("%s", buff);
-	
+	fprintf(stdout, "123\ncctv");
+	fprintf(stderr, buf);
+	sleep(2);
 	//fflush(stdout);
 
 	return 0;
 
 }
+
